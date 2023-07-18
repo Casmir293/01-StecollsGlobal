77f1d6421ae93b169964f32304e489932584b73d
Casmir Onyeka <casmir293@gmail.com>
2023-07-18 22:05:23 +0100

    Menu Change Color on Scroll.

---
+++ dist/js/scroll.js
@@ -0,0 +1,12 @@
+// Nav Scroll Animation
+window.onscroll = () => {
+  const nav = document.querySelector("#scrollId");
+  const collapsibleNavbar = document.querySelector("#collapsibleNavbar");
+  if (this.scrollY <= 10) {
+    nav.classList.remove("scroll");
+    collapsibleNavbar.classList.remove("myStrokeScroll");
+  } else {
+    nav.classList.add("scroll");
+    collapsibleNavbar.classList.add("myStrokeScroll");
+  }
+};
